07c75d5d71a8e2e7fb9b9eaf13e2a167ec3e2320
tiheryv <tiheryvr@gmail.com>
2024-11-12 18:34:30 -0600

    Day 2 - Synchronous and Asynchrnous code

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -9,3 +9,18 @@ console.log(textInp)
 const textOut = `This is what we know about the avocado: ${textInp}.\nCreated on ${Date.now()}`;
 fs.writeFileSync('./txt/output.txt', textOut);
 console.log("File written!")
+
+
+//Non-blocking Asynchronoues way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, (err) => {
+                console.log("Your file has been written")
+            })
+        })
+    })
+})
+
+console.log("Will read the file")
