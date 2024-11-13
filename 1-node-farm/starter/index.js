11776457dc3d01d37656806faf06ee39d8315322
tiheryv <tiheryvr@gmail.com>
2024-11-12 21:25:44 -0600

    Day 2 - Simple mocked API

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -4,6 +4,7 @@
 const fs = require('fs');
 const http = require('http');
 const path = require('path');
+const { json } = require('stream/consumers');
 const url = require('url');
 
 // const textInp = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -29,13 +30,52 @@ const url = require('url');
 // console.log("Will read the file")
 
 //Creating a simple Web Server
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const objData = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     const pathname = req.url;
-
+    //OVERVIEW PAGE
     if (pathname == "/overview" || pathname == "/") {
-        res.end('This is overview')
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
+
+        const cardsHtml = objData.map(elem => replaceTemplate(tempCard, elem)).join('')
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+        res.end(output)
     } else if (pathname == "/product") {
         res.end('This is product');
+    } else if (pathname == "/api") {
+        fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+            const productData = objData;
+            console.log(productData)
+            res.writeHead(200, {
+                'Content-type': 'application/json'
+            });
+            res.end('API');
+
+        })
     } else {
         //Network Headers
         res.writeHead(404, {
