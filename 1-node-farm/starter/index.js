700f2cb502dd51988010e58b1d9a5a6455fcd83a
tiheryv <tiheryvr@gmail.com>
2024-11-12 20:28:16 -0600

    Day 2 - Simple web server and routing

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,25 +2,50 @@
 
 // Reading and writing files
 const fs = require('fs');
+const http = require('http');
+const path = require('path');
+const url = require('url');
 
-const textInp = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textInp)
+// const textInp = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textInp)
 
-const textOut = `This is what we know about the avocado: ${textInp}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log("File written!")
+// const textOut = `This is what we know about the avocado: ${textInp}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log("File written!")
 
 
-//Non-blocking Asynchronoues way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+// //Non-blocking Asynchronoues way
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
 
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, (err) => {
-                console.log("Your file has been written")
-            })
-        })
-    })
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, (err) => {
+//                 console.log("Your file has been written")
+//             })
+//         })
+//     })
+// })
+
+// console.log("Will read the file")
+
+//Creating a simple Web Server
+const server = http.createServer((req, res) => {
+    const pathname = req.url;
+
+    if (pathname == "/overview" || pathname == "/") {
+        res.end('This is overview')
+    } else if (pathname == "/product") {
+        res.end('This is product');
+    } else {
+        //Network Headers
+        res.writeHead(404, {
+            'Content-type': 'text/html'
+        });
+        res.end('<h1>Not found</h1>');
+    }
 })
 
-console.log("Will read the file")
+//(port, localhost(default))
+server.listen(8000, '127.0.0.1', () => {
+    console.log("Listening to requests on port 8000")
+})
